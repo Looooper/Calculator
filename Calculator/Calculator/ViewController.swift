a272928c18e4f32c8b25df34779c1eddcc430106
Looooper <shadowfiend820@outlook.com>
2016-02-24 19:22:23 +0800

    implementation

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: UIViewController {
     
     var userIsInTypingANumber = false
     
+    var displayValue:Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+        }
+    }
+    
+    var calculatorBrain = CalculatorBrain()
+    
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTypingANumber{
@@ -26,18 +37,49 @@ class ViewController: UIViewController {
     }
     @IBAction func enter() {
         userIsInTypingANumber = false
-        
-    }
-    
-    @IBAction func performOperate(sender: UIButton) {
-        if let symbol = sender.currentTitle{
-            switch symbol{
-            case "+":
-            case "−":
-            case "×":
-            case "÷":
-            }
+        //        opStack.append(displayValue)
+        //        print(opStack)
+        if let result = calculatorBrain.pushOperand(displayValue){
+            displayValue = result
+        } else {
+            displayValue = 0
         }
     }
+    
+    //    var opStack = Array<Double>()
+    
+    @IBAction func operate(sender: UIButton) {
+        let symbol = sender.currentTitle!
+        if userIsInTypingANumber{
+            enter()
+        }
+        if let result = calculatorBrain.performOperation(symbol){
+            displayValue = result;
+        } else {
+            displayValue = 0
+        }
+        //        switch symbol{
+        //        case "+":performOperation {$1 + $0}
+        //        case "−":performOperation {$1 - $0}
+        //        case "×":performOperation {$0 * $1}
+        //        case "÷":performOperation {$1 / $0}
+        //        case "√":performOperation {sqrt($0)}
+        //        default:break
+        //        }
+    }
+    
+    //    func performOperation (operation:(Double,Double)->Double){
+    //        if opStack.count >= 2{
+    //            displayValue = operation(opStack.removeLast(),opStack.removeLast())
+    //            enter()
+    //        }
+    //    }
+    
+    //    func performOperation (operation:Double->Double){
+    //        if opStack.count >= 1{
+    //            displayValue = operation(opStack.removeLast())
+    //            enter()
+    //        }
+    //    }
 }
 
